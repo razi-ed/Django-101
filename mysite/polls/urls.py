f13e189ed46aa98dbd77451958d1b24fe00a89ca
Razi <razi.abdulrasheed@mountblue.io>
2018-02-12 10:20:10 +0530

    Complete tutorial-3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    url(r'^awesome/$',views.index,name='index'),
-    url(r'^totally-cool/$', views.t_c, name="t_c")
+    url(r'^$',views.index,name="index"),
+    url(r'^totally-cool/$', views.t_c, name="t_c"),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.result, name='result'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote")
     ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,Http404
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Razi's Awesome App")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def t_c(request):
     return HttpResponse("This is totally Cooool!")
+
+def detail(request, question_id):
+    # try:
+        # question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+        # raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    message = "You're voting on question %s."
+    return HttpResponse(message % question_id)
